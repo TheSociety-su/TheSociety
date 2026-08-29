3164ee6e018b27574d8e2c5c4c6fec894c31d039
TheSociety-su <Thesociety2709@gmail.com>
2026-08-29 11:41:07 +0500

    Update button label from 'Mening registratsiyalarim' to 'Profil'

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -9,7 +9,7 @@ from app.config import EVENT_FORMATS, INTEREST_CATEGORIES
 from app.models import Event, University
 
 BTN_EVENTS = "📅 Eventlar"
-BTN_MY = "🗒 Mening registratsiyalarim"
+BTN_MY = "👤 Profil "
 
 
 def main_menu_kb() -> ReplyKeyboardMarkup:
